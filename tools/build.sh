2e4658109dd3d39c51155e5a3bcb669852ce536a
Thomas Seliger <neovatar@gmail.com>
2026-07-16 21:52:38 +0200

    Revert to state at 6d1eed4af4c68fbc290f93790846deadb7674e57

--- tools/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-echo
-echo "Building dist/more-text-options.zip"
-mkdir -p dist
-zip -r dist/more-text-options.zip module.json scripts README.md LICENSE.md
-
--- tools/update-files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-echo "Updating module.json with version $SEMREL_NEXT_VERSION"
-jq --arg version "$SEMREL_NEXT_VERSION" '.version = $version | .download = "https://github.com/neovatar/more-text-options/releases/download/release-\($version)/more-text-options.zip"' module.json > module.tmp.json && mv module.tmp.json module.json
-
-echo "Updating README.md with version $SEMREL_NEXT_VERSION"
-sed -i -E "/Downloads \(v/s/v[0-9]+\.[0-9]+\.[0-9]+/${SEMREL_NEXT_VERSION}/g" README.md
-
